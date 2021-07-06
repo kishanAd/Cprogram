7e17b27ddea8ac86038dbffb47f957eba9024a3c
Kishan Adhikari <61057393+kishanAd@users.noreply.github.com>
2021-07-06 22:04:42 +0545

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+cache={}
+def fibonacci(num):
+    if num in cache:
+        return cache[num]
+    if num==1:
+        return 1
+    elif num==2:
+        return 2
+
+    elif num>2:
+        result= fibonacci(num-1)+fibonacci(num-2)
+    cache[num]=result
+    return result
+count=0
+for i in range(1,11):
+
+    if fibonacci(i)%2==0:
+        count+=fibonacci(i)
+
+print(count)
